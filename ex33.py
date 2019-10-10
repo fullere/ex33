19cfa6357a926c40f2cfaf94da2ec953379af056
fullere <fullere@hartwick.edu>
2019-10-09 23:54:23 -0400

    added code

--- ex33.py
+++ ex33.py
@@ -1,2 +1,17 @@
 # Elizabeth Fuller
 # 10/9/19
+# while loops
+
+i = 0
+numbers = []
+while i < 6:
+    print(f"At yje top i is {i}")
+    numbers.append(i)
+    i = i + 1
+    print(f"Numbers now: ", numbers)
+    print(f"At the bottom i is {i}")
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
